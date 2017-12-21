1b088d86717436001e2461f29ad42b90549eb40c
Katerina Gkirtzou <katerina.gkirtzou@gmail.com>
2017-12-21 17:10:55 +0200

    Finished generate controled vocabulary from owl to xsd

--- owl2xml/generate_controlled_vocabularies.py
+++ owl2xml/generate_controlled_vocabularies.py
@@ -1,6 +1,63 @@
 import lxml.etree as etree
-# from lxml.etree import Element, SubElement, QName, tostring
 import rdflib
+
+
+def recursiveFunction(element, uriResource, rdfGraph):
+    resource = rdfGraph.resource(uriResource)
+    parentResources = resource.objects(rdflib.RDFS.subClassOf)
+
+    print "Working resource is " + resource.identifier
+    # for parent in parentResources:
+    #    print "and has parent " + parent.identifier
+
+    # Create xsd enumeration element
+    # rdf URI to enumeration value
+    enumeration = etree.SubElement(
+        element, etree.QName(xs, 'enumeration'))
+    enumeration.attrib[etree.QName('value')] = resource.identifier
+    # xs:annotation
+    enumerationAnnotation = etree.SubElement(
+        enumeration, etree.QName(xs, 'annotation'))
+    # rdfs:comment to xs:documentation
+    enumerationDocumentation = etree.SubElement(
+        enumerationAnnotation, etree.QName(xs, 'documentation'))
+    enumerationDocumentation.text = resource.comment()
+    # xs:appinfo
+    enumerationAppInfo = etree.SubElement(
+        enumerationAnnotation, etree.QName(xs, 'appinfo'))
+    # rdfs:label to label
+    enumerationLabel = etree.SubElement(
+        enumerationAppInfo, etree.QName('label'))
+    enumerationLabel.text = resource.label()
+
+    # rdfs:subclassOf to subclassOf
+    for parent in parentResources:
+        print "and has parent " + parent.identifier
+        if parent.identifier.startswith('http'):
+            enumerationSubclassOf = etree.SubElement(
+                enumerationAppInfo, etree.QName('subclassOf'))
+            enumerationSubclassOf.text = parent.identifier
+
+    enumerationAltLabels = resource.objects(rdflib.namespace.SKOS.altLabel)
+    for altlabel in enumerationAltLabels:
+        # skos:altLabel to altLabel
+        enumerationAltLabel = etree.SubElement(
+            enumerationAppInfo, etree.QName('altLabel'))
+        enumerationAltLabel.text = altlabel
+
+    subclasses = resource.subjects(rdflib.RDFS.subClassOf)
+
+    for c in subclasses:
+        # Ignore myself resource
+        if c.identifier == resource.identifier:
+            continue
+        recursiveFunction(element, c.identifier, rdfGraph)
+    return
+
+# Read xsd file
+tree = etree.parse('OMTD-SHARE-ControlledVocabs.xsd')
+root = tree.getroot()
+# print etree.tostring(root, pretty_print=True)
 
 # Declaring namespace
 xs = 'http://www.w3.org/2001/XMLSchema'
@@ -12,82 +69,50 @@ target_version = '3.0.1'
 
 
 # Create xsd root element
-root = etree.Element(etree.QName(xs, 'schema'),
-                     nsmap={'ms': target_ns, 'xs': xs})
-root.attrib[etree.QName('targerNamespace')] = target_ns
-root.attrib[etree.QName('elementFormDefault')] = 'qualified'
-root.attrib[etree.QName('attributeFormDefault')] = 'unqualified'
-root.attrib[etree.QName('version')] = target_version
-root.attrib[etree.QName(xml, 'lang')] = 'en'
+# root = etree.Element(etree.QName(xs, 'schema'),
+#                     nsmap={'ms': target_ns, 'xs': xs})
+# root.attrib[etree.QName('targerNamespace')] = target_ns
+# root.attrib[etree.QName('elementFormDefault')] = 'qualified'
+# root.attrib[etree.QName('attributeFormDefault')] = 'unqualified'
+# root.attrib[etree.QName('version')] = target_version
+# root.attrib[etree.QName(xml, 'lang')] = 'en'
 
 
 # Read OWL ontology file
-graph = rdflib.Graph()
-result = graph.parse("omtd-share-tdm-ontology.rdf",
-                     format="application/rdf+xml")
-print('graph has %s statements.' % len(graph))
+rdfGraph = rdflib.Graph()
+result = rdfGraph.parse("omtd-share-tdm-ontology.rdf",
+                        format="application/rdf+xml")
+print('graph has %s statements.' % len(rdfGraph))
 
-# Create xsd controlledVocabulary element
-# Get RDF Class
-uriControlledVocabulary = 'http://w3id.org/meta-share/omtd-share/AnnotationType'
-resourceControlledVocabulary = graph.resource(uriControlledVocabulary)
-print('ControlledVocabulary :: ' + str(resourceControlledVocabulary))
-print resourceControlledVocabulary.identifier
-
-controlledVocabulary = etree.SubElement(root, etree.QName(xs, 'simpleType'))
-controlledVocabulary.attrib[etree.QName(
-    'name')] = resourceControlledVocabulary.identifier
-cvAnnotation = etree.SubElement(
-    controlledVocabulary, etree.QName(xs, 'annotation'))
-cvDocumentation = etree.SubElement(
-    cvAnnotation, etree.QName(xs, 'documentation'))
-cvDocumentation.text = resourceControlledVocabulary.comment()
-cvAppInfo = etree.SubElement(
-    cvAnnotation, etree.QName(xs, 'appinfo'))
-cvLabel = etree.SubElement(cvAppInfo, etree.QName('label'))
-cvLabel.text = resourceControlledVocabulary.label()
-cvRestriction = etree.SubElement(
-    controlledVocabulary, etree.QName(xs, 'restriction'))
-cvRestriction.attrib[etree.QName('base')] = 'xs:URI'
-
-parent = resourceControlledVocabulary
-subclasses = list(
-    parent.subjects(rdflib.RDFS.subClassOf))
-# subclasses = list(graph.subjects(
-#    rdflib.RDF.type, uriControlledVocabulary))
-print 'Subclasses size is ' + str(subclasses.__len__())
-for c in subclasses:
-    print c
-
-c = subclasses[1]
-# rdf URI to enumeration value
-enumerationCV = etree.SubElement(cvRestriction, etree.QName(xs, 'enumeration'))
-enumerationCV.attrib[etree.QName('value')] = c.identifier
-# xs:annotation
-enumerationCVAnnotation = etree.SubElement(
-    enumerationCV, etree.QName(xs, 'annotation'))
-# rdfs:comment to xs:documentation
-enumerationCVDocumentation = etree.SubElement(
-    enumerationCVAnnotation, etree.QName(xs, 'documentation'))
-enumerationCVDocumentation.text = c.comment()
-# xs:appinfo
-enumerationCVAppInfo = etree.SubElement(
-    enumerationCVAnnotation, etree.QName(xs, 'appinfo'))
-# rdfs:label to label
-enumerationCVLabel = etree.SubElement(
-    enumerationCVAppInfo, etree.QName('label'))
-enumerationCVLabel.text = c.label()
-# rdfs:subclassOf to subclassOf
-enumerationCVSubclassOf = etree.SubElement(
-    enumerationCVAppInfo, etree.QName('subclassOf'))
-enumerationCVSubclassOf.text = parent.identifier
-
-enumerationCValtLabels = list(c.objects(rdflib.namespace.SKOS.altLabel))
-for altlabel in enumerationCValtLabels:
-    # skos:altLabel to altLabel
-    enumerationCValtLabel = etree.SubElement(
-        enumerationCVAppInfo, etree.QName('altLabel'))
-    enumerationCVAltLabel.text = altlabel
+for element in root.iter(etree.QName(xs, 'simpleType').text):
+    print("%s - %s" % (element.tag, element.attrib['name']))
+    subelement = list(element.iter(etree.QName(xs, 'restriction').text))
+    assert len(subelement) == 1
+    subelement = subelement[0]
+    uriResources = []
+    # Get RDF Class
+    if element.attrib['name'] == 'dataFormatType':
+        uriResources.append('http://w3id.org/meta-share/omtd-share/DataFormat')
+    elif element.attrib['name'] == 'annotationTypeType':
+        uriResources.append(
+            'http://w3id.org/meta-share/omtd-share/AnnotationType')
+    elif element.attrib['name'] == 'TDMMethodType':
+        uriResources.append('http://w3id.org/meta-share/omtd-share/TdmMethod')
+    elif element.attrib['name'] == 'operationType':
+        uriResources.append(
+            'http://w3id.org/meta-share/omtd-share/ComponentType')
+        uriResources.append('http://w3id.org/meta-share/omtd-share/Annotation')
+        uriResources.append(
+            'http://w3id.org/meta-share/omtd-share/TextAndDataMining')
+    else:
+        continue
+    for uriResource in uriResources:
+        recursiveFunction(subelement, uriResource, rdfGraph)
 
 
-print etree.tostring(root, pretty_print=True)
+# print etree.tostring(root, pretty_print=True)
+
+# Print xml
+fileXML = open("foo.xsd", 'w')
+fileXML.write(etree.tostring(root, pretty_print=True))
+fileXML.close()

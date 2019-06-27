bbab77d51c72251fffd5752e820787b065702159
Katerina Gkirtzou <katerina.gkirtzou@gmail.com>
2019-06-27 15:58:08 +0300

    Fix in owl2xml.generate_xsd_elements generation dict from object properties as well

--- owl2xml/generate_xsd_elements.py
+++ owl2xml/generate_xsd_elements.py
@@ -1,19 +1,43 @@
 import ConfigParser
+import pprint
 
 import rdflib
-from rdflib import Literal
+
+
+def literal_to_tuple(literal):
+    return (literal.value, literal.language)
 
 
 def literals_to_list(literals):
-    return [(l.value, l.language) for l in literals]
+    return [literal_to_tuple(l) for l in literals]
+
 
 def resources_to_list(resources):
-    return [r for r in resources]
+    return [str(r.identifier) for r in resources]
+
+
+def resource_common_elements_to_dict(resource):
+    dict_r = dict()
+    dict_r['identifier'] = str(resource.identifier)
+    try:
+        dict_r['name'] = resource.qname()
+    except:
+        pass
+    dict_r['def'] = literals_to_list(resource.objects(rdflib.namespace.RDFS.comment))
+    dict_r['label'] = literals_to_list(resource.objects(rdflib.namespace.RDFS.label))
+    dict_r['label_plural'] = []
+    dict_r['alt_label'] = literals_to_list(resource.objects(rdflib.namespace.SKOS.altLabel))
+    dict_r['example'] = literals_to_list(resource.objects(rdflib.namespace.SKOS.example))
+    dict_r['close_match'] = resources_to_list(resource.objects(rdflib.namespace.SKOS.closeMatch))
+    dict_r['exact_match'] = resources_to_list(resource.objects(rdflib.namespace.SKOS.exactMatch))
+    dict_r['broad_match'] = resources_to_list(resource.objects(rdflib.namespace.SKOS.broadMatch))
+    dict_r['narrow_match'] = resources_to_list(resource.objects(rdflib.namespace.SKOS.narrowMatch))
+    return dict_r
+
 
 if __name__ == '__main__':
     Config = ConfigParser.ConfigParser()
     Config.read('generate_xsd_elements.ini')
-
 
     filename_owl = Config.get('Input', 'filename_owl')
     filename_xsd = Config.get('Output', 'filename_xsd')
@@ -22,83 +46,94 @@ if __name__ == '__main__':
     rdfGraph = rdflib.Graph()
     rdfGraph.parse(filename_owl)
 
+    pp = pprint.PrettyPrinter(indent=4)
     dict_for_xml = []
     # For DatatypeProperties
     data_properties_results = rdfGraph.query(
         """
         SELECT ?p
 	    WHERE { ?p a owl:DatatypeProperty }
-	    ORDER BY ASC(?p)
-	    LIMIT 10
+	    ORDER BY ASC(?p)	    
         """)
 
     for res in data_properties_results:
         resource = rdfGraph.resource(res['p'])
 
-        data_property = dict()
+        data_property = resource_common_elements_to_dict(resource)
         data_property['property'] = 'dataProp'
         # More thought when range is or combination
         range = resource.value(rdflib.namespace.RDFS.range)
+        ## TODO Data property with complex combined range
+        # r = rdfGraph.resource('http://purl.org/net/def/metashare#contactPoint').value(rdflib.namespace.RDFS.range)
+        # print(r)
         if range != None:
-            data_property['type'] = range.qname()
+            try:
+                data_property['type'] = range.qname()
+            except:
+                data_property['type'] = None
         else:
             data_property['type'] = None
-        data_property['name'] = resource.qname()
-        data_property['def'] = literals_to_list(resource.objects(rdflib.namespace.RDFS.comment))
-        data_property['label'] = literals_to_list(resource.objects(rdflib.namespace.RDFS.label))
-        data_property['label_plural'] = []
-        data_property['alt_labels'] = literals_to_list(resource.objects(rdflib.namespace.SKOS.altLabel))
-        data_property['example'] = literals_to_list(resource.objects(rdflib.namespace.SKOS.example))
-        data_property['close_match'] = resources_to_list(resource.objects(rdflib.namespace.SKOS.closeMatch))
-        data_property['exact_match'] = resources_to_list(resource.objects(rdflib.namespace.SKOS.exactMatch))
-        data_property['broad_match'] = resources_to_list(resource.objects(rdflib.namespace.SKOS.broadMatch))
-        data_property['narrow_match'] = resources_to_list(resource.objects(rdflib.namespace.SKOS.narrowMatch))
 
         dict_for_xml.append(data_property)
 
-    for d in dict_for_xml:
-         print(d)
-
-    r = rdfGraph.resource('http://purl.org/net/def/metashare#contactPoint').value(rdflib.namespace.RDFS.range)
-
-    print(r)
-
     # For ObjectProperties
-    # qres = rdfGraph.query(
-    #    """
-    #    SELECT ?p
-    #    WHERE { ?p a owl:ObjectProperty }
-    #    ORDER BY ASC(?p)
-    #    """)
+    obj_properties_results = rdfGraph.query(
+        """
+        SELECT ?p
+        WHERE { ?p a owl:ObjectProperty }
+        ORDER BY ASC(?p)       
+        """)
+
+    for res in obj_properties_results:
+        resource = rdfGraph.resource(res['p'])
+        range = resource.value(rdflib.namespace.RDFS.range)
+        if range != None:
+            query_class_instances = "SELECT ?i WHERE { ?i a <" + range.identifier + ">} ORDER BY ASC(?i)"
+            ci_res = rdfGraph.query(query_class_instances)
+            # Case 1: CV the instances of the class range
+            if len(ci_res) > 0:
+                dict_r = resource_common_elements_to_dict(range)
+                dict_r['property'] = 'objProp'
+                dict_r['type'] = range.qname()
+                dict_r['controlled_vocabulary'] = []
+                for ci in ci_res:
+                    instance = rdfGraph.resource(ci['i'])
+                    dict_r['controlled_vocabulary'].append(resource_common_elements_to_dict(instance))
+                dict_for_xml.append(dict_r)
+            else:
+                query_subclasses = "SELECT ?sc WHERE { ?sc rdfs:subClassOf <" + range.identifier + ">} ORDER BY ASC(?sb)"
+                sc_res = rdfGraph.query(query_subclasses)
+                # Case 2: CV the instances of the classes, where classes are subclasses of class range
+                if len(sc_res) > 0:
+                    for sc in sc_res:
+                        subclass = rdfGraph.resource(sc['sc'])
+                        query_class_instances = "SELECT ?i WHERE { ?i a <" + subclass.identifier + ">} ORDER BY ASC(?i)"
+                        ci_res = rdfGraph.query(query_class_instances)
+                        dict_r = resource_common_elements_to_dict(subclass)
+                        dict_r['property'] = 'objProp'
+                        dict_r['type'] = range.qname()
+                        dict_r['controlled_vocabulary'] = []
+                        for ci in ci_res:
+                            instance = rdfGraph.resource(ci['i'])
+                            dict_r['controlled_vocabulary'].append(resource_common_elements_to_dict(instance))
+                        dict_for_xml.append(dict_r)
+                # Case 3: no CV
+                else:
+                    dict_r = resource_common_elements_to_dict(range)
+                    dict_r['property'] = 'objProp'
+                    try:
+                        dict_r['type'] = range.qname()
+                    except:
+                        dict_r['type'] = None
+                    dict_r['controlled_vocabulary'] = []
+                    dict_for_xml.append(dict_r)
+
+    out = open(filename_xsd, 'w')
+    for e in dict_for_xml:
+        out.write(str(e) + '\n')
+    out.close()
+
     #
-    # for row in qres:
-    #     resource = rdfGraph.resource(row['p'])
     #
-    #     print(resource)
-    #     range = resource.value(rdflib.namespace.RDFS.range)
-    #     if range != None:
-    #         rr = rdfGraph.resource(range)
-    #         print('range resource', rr.label())
-    #
-    #     # print(resource.qname())
-    #     # print('Label', resource.label())
-    #     # print('Def', resource.comment())
-    #     #
-    #     # print('AltLabels', [(altL.value, altL.language) for altL in resource.objects(rdflib.namespace.SKOS.altLabel)])
-    #     # print('Example', [ex for ex in resource.objects(rdflib.namespace.SKOS.example)])
-    #     # print('CloseMatch', [ex for ex in resource.objects(rdflib.namespace.SKOS.closeMatch)])
-    #     # print('ExactMatch', [ex for ex in resource.objects(rdflib.namespace.SKOS.exactMatch)])
-    #     # print('BroadMatch', [ex for ex in resource.objects(rdflib.namespace.SKOS.broadMatch)])
-    #     # print('NarrowMatch', [ex for ex in resource.objects(rdflib.namespace.SKOS.narrowMatch)])
-
-
-
-    # print etree.tostring(root, pretty_print=True)
-
-    # Print xml
-    #tree = etree.parse(filename_xsd)
-    #fileXML = open(filename_xsd, 'w')
-    #ileXML.write(etree.tostring(root, pretty_print=True))
-    #fileXML.close()
-
-
+    # for d in dict_for_xml:
+    #       pp.pprint(d)

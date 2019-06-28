e7c76115700de2179ace1d127815e187dbeb5771
MiltosD <mdeligiannis@outlook.com>
2019-06-28 12:07:31 +0300

    python3 compatibility

--- owl2pythonDic/generate_dictionary.py
+++ owl2pythonDic/generate_dictionary.py
@@ -1,23 +1,23 @@
+import configparser
+
 import rdflib
-import ConfigParser
+
 
 def generate_dictionary(choices, uriResource, rdfGraph, alreadySeen):
-
     if uriResource in alreadySeen:
-        print 'Already seen ' + uriResource
+        print('Already seen ' + uriResource)
         return
     alreadySeen.append(uriResource)
 
     resource = rdfGraph.resource(uriResource)
     parentResources = resource.objects(rdflib.RDFS.subClassOf)
 
-    print "Working resource is " + resource.identifier
-    #for parent in parentResources:
+    print("Working resource is " + resource.identifier)
+    # for parent in parentResources:
     #    print "and has parent " + parent.identifier
 
     pair = (resource.identifier.__str__(), resource.label().__str__())
     choices.append(pair)
-
 
     subclasses = resource.subjects(rdflib.RDFS.subClassOf)
 
@@ -29,9 +29,8 @@ def generate_dictionary(choices, uriResource, rdfGraph, alreadySeen):
     return
 
 
-
 if __name__ == '__main__':
-    Config = ConfigParser.ConfigParser()
+    Config = configparser.ConfigParser()
     Config.read('generate_dictionary.ini')
 
     filename_owl = Config.get('Input', 'filename_owl')
@@ -48,7 +47,7 @@ if __name__ == '__main__':
     for m in mapping:
         root_url = Config.get('Mapping', m[0])
 
-        print root_url
+        print(root_url)
         choices = []
         generate_dictionary(choices, root_url, rdfGraph, [])
 

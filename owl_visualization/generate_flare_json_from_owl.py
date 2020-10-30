81eafbc1c9c63d85d4a0503a884776e9516291f9
Katerina Gkirtzou <katerina.gkirtzou@gmail.com>
2020-10-30 16:59:52 +0200

    GenerateFlare: Update script to work on python3.7 and rdflib=5.0.0

--- owl_visualization/generate_flare_json_from_owl.py
+++ owl_visualization/generate_flare_json_from_owl.py
@@ -1,7 +1,9 @@
-import ConfigParser
+import configparser
 import rdflib
 import jsonpickle
 import sys
+import json
+
 sys.setrecursionlimit(100000)
 
 
@@ -13,24 +15,25 @@ class Flare:
         self.definition = definition
         self.children = []
 
-    def __str__(self):
-        str_representation = "name=%s URI=%s definition=%s children=[" % (
-            self.name, self.URI, self.definition)
-        for child in self.children:
-            str_representation = str_representation + str(child) + ', '
-        str_representation = str_representation + ']'
-        return str_representation
+    # def __str__(self):
+    #     str_representation = "name=%s URI=%s definition=%s children=[" % (
+    #         self.name, self.URI, self.definition)
+    #     for child in self.children:
+    #         str_representation = str_representation + str(child) + ', '
+    #     str_representation = str_representation + ']'
+    #     return str_representation
 
     def add_child(self, child):
         self.children.append(child)
 
 
 def createFlareObj(workingClassURI, rdfGraph, deprecated_classes):
+    print(f'Working on: {workingClassURI}')
     workingClass = rdfGraph.resource(workingClassURI)
     # Create flare object for the working OWL class
-    wcName = workingClass.label().encode('utf-8')
-    wcURI = workingClassURI.encode('utf-8')
-    wcDefinition = workingClass.comment().encode('utf-8')
+    wcName = str(workingClass.label())
+    wcURI = str(workingClassURI)
+    wcDefinition = str(workingClass.comment())
     # print wcDefinition
     flare_obj = Flare(wcName, wcURI, wcDefinition)
     # print(flare_obj)
@@ -39,14 +42,15 @@ def createFlareObj(workingClassURI, rdfGraph, deprecated_classes):
     query = "SELECT DISTINCT ?c  WHERE { ?c a owl:Class. ?c rdfs:subClassOf <%s>}" % workingClassURI
     qres = rdfGraph.query(query)
 
-    print 'Working with class:', workingClassURI,  'which has ', len(qres), ' children'
+    print(f'Working with class: {workingClassURI} which has {len(qres)} children')
     for row in qres:
         # Get subclass URI
-        workingSubClassURI = row['c'].encode('utf-8')
+        workingSubClassURI = row['c']
         if workingSubClassURI in deprecated_classes:
-            print workingClassURI, ' is deprecated. Ignore'
+            print(f'{workingClassURI} is deprecated. Ignore')
         else:
             # Create subclass flare object
+            print(f'Working on child: {workingSubClassURI}')
             flare_obj_child = createFlareObj(
                 workingSubClassURI, rdfGraph, deprecated_classes)
             # Add subclass flare object to working class flare object
@@ -56,12 +60,13 @@ def createFlareObj(workingClassURI, rdfGraph, deprecated_classes):
 
 
 if __name__ == '__main__':
-    Config = ConfigParser.ConfigParser()
+    Config = configparser.ConfigParser()
     Config.read('generate_flare_json_from_owl.ini')
 
     filename_owl = Config.get('Input', 'filename_owl')
     filename_owl_format = Config.get('Input', 'filename_owl_format')
     filename_json = Config.get('Output', 'filename_json')
+    url_prefix = Config.get('Input', 'url_prefix')
 
     # Read OWL ontology file
     rdfGraph = rdflib.Graph()
@@ -75,7 +80,8 @@ if __name__ == '__main__':
         ?c rdfs:subClassOf owl:Thing
         }""")
     ontology_classes = [
-        x['c'] for x in qres if 'http://w3id.org/meta-share/omtd-share/' in x['c']]
+        x['c'] for x in qres if url_prefix in x['c']]
+    ontology_classes.sort()
 
     # Get deprecated classes
     qres = rdfGraph.query(
@@ -85,19 +91,20 @@ if __name__ == '__main__':
             ?deprecatedClass owl:deprecated "true"^^<http://www.w3.org/2001/XMLSchema#boolean>
         } """)
     deprecated_classes = [
-        x['deprecatedClass'].encode('utf-8') for x in qres if 'http://w3id.org/meta-share/omtd-share/' in x['deprecatedClass']]
+        x['deprecatedClass'].encode('utf-8') for x in qres if url_prefix in x['deprecatedClass']]
     print('Deprecated classes', deprecated_classes)
 
     flare_root = Flare('OWL-Thing', '', '')
+
     # For each OWL Thing subclass, ie workingClass
     for workingClassURI in ontology_classes:
         if workingClassURI not in deprecated_classes:
-            print 'Working on ', workingClassURI.encode('utf-8'), ' branch'
+            print(f'Working on {workingClassURI.encode("utf-8")} branch')
             flare_obj = createFlareObj(
                 workingClassURI, rdfGraph, deprecated_classes)
             flare_root.add_child(flare_obj)
         else:
-            print workingClassURI, ' is deprecated. Ignore'
+            print(f'{workingClassURI} is deprecated. Ignore')
 
     # print(flare_root)
     # print(jsonpickle.encode(flare_root))
@@ -107,3 +114,4 @@ if __name__ == '__main__':
     output = open(filename_json, 'w')
     output.write(json_data)
     output.close()
+
